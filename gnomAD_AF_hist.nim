4535f946d857a3726ad8cc86ad7b43074b633e8c
BrianLohman <brian.keith.lohman@gmail.com>
2018-12-12 13:44:40 -0700

    record low impact and highest of mutliple impacts

--- gnomAD_AF_hist.nim
+++ gnomAD_AF_hist.nim
@@ -17,14 +17,16 @@ var
   found = 0
   not_in_gnomad = 0
   in_gnomad = 0
-  high_out = open(commandLineParams()[0]&"_high_impact_gnomAD_AF.txt", fmWrite)
-  med_out = open(commandLineParams()[0]&"_med_impact_gnomAD_AF.txt", fmWrite)
+  fh = commandLineParams()[0]
+  high_out = open(fh[0..^5]&"_high_impact_gnomAD_AF.txt", fmWrite)
+  med_out = open(fh[0..^5]&"_med_impact_gnomAD_AF.txt", fmWrite)
+  low_out = open(fh[0..^5]&"_low_impact_gnomAD_AF.txt", fmWrite)
 
 type Impact = enum  
+  UNKNOWN = -1
   LOW = 0  
   MED = 1  
   HIGH = 2  
-  UNKNOWN = 3
 
 # define impact levels by VEP annotation as a set
 var low_impacts = toSet(@["3_prime_UTR_truncation","non_canonical_start_codon","synonymous_variant","coding_sequence_variant","incomplete_terminal_codon_variant","stop_retained_variant","mature_miRNA_variant","5_prime_UTR_premature_start_codon_variant","5_prime_UTR_premature_start_codon_gain_variant","5_prime_UTR_variant","3_prime_UTR_variant","non_coding_transcript_exon_variant","conserved_intron_variant","intron_variant","exon_variant","gene_variant","NMD_transcript_variant","non_coding_transcript_variant","upstream_gene_variant","downstream_gene_variant","TFBS_ablation","TFBS_amplification","TF_binding_site_variant","regulatory_region_amplification","feature_elongation","miRNA","transcript_variant","start_retained","regulatory_region_variant","feature_truncation","non_coding_exon_variant","nc_transcript_variant","conserved_intergenic_variant","intergenic_variant","intergenic_region","intragenic_variant","non_coding_transcript_exon_variant","non_coding_transcript_variant","transcript","sequence_feature","non_coding"])
@@ -55,35 +57,40 @@ for variant in vcf:
   var
     gnomad_af = newSeq[float32]()
     status = variant.info.get("gnomAD_AF", gnomad_af)
-    impact: Impact
 
   if status == Status.OK:
     in_gnomad += 1
 
   if status != Status.OK:
     not_in_gnomad += 1
-    gnomad_af = 0
+    gnomad_af = @[0'f32]
 
+  var max_impact = Impact.UNKNOWN
   for a in annotation.split(","):
     var asp = a.split("|")
     for imp in asp[1].split('&'):
-      if imp in low_impacts:
+      if imp in unknown_impacts:
         continue
+      elif imp in low_impacts:
+        max_impact = max(max_impact, Impact.LOW)
       elif imp in med_impacts:
-        impact = Impact.MED
+        max_impact = max(max_impact, Impact.MED)
       elif imp in high_impacts:
-        impact = Impact.HIGH
-      elif imp in unknown_impacts:
-        impact = Impact.UNKNOWN
+        max_impact = Impact.HIGH
       else:
         quit "unknown impact: " & imp
   
+  var impact = max_impact
   if impact == Impact.UNKNOWN:
     continue
     
   elif impact == Impact.LOW:
     continue
     
+  elif impact == Impact.LOW:
+    low_out.write_line(gnomad_af[0])
+    continue
+  
   elif impact == Impact.MED:
     med_out.write_line(gnomad_af[0])
     continue
